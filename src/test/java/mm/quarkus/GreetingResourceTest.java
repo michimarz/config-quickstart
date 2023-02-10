40be1351cb3698e408676620d8fe7da2a1a54b70
Michi Marz <michimarzo@gmail.com>
2023-02-10 22:12:14 +0100

    working app

---
+++ src/test/java/mm/quarkus/GreetingResourceTest.java
@@ -0,0 +1,18 @@
+package mm.quarkus;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    // @Test
+    // public void testHelloEndpoint() {
+    //     given()
+    //       .when().get("/greeting")
+    //       .then()
+    //          .statusCode(200)
+    //          .body(is("hello quarkus!")); // Modified line
+    // }
+
+}
